c561ce2da56eb19d06580d0106d890b98e6d761a
woozlic <sampaccs@mail.ru>
2021-01-19 22:00:45 +0500

    added change password form

--- account/urls.py
+++ account/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
 from . import views
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
 app_name = 'account'
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('password_change/', auth_views.PasswordChangeView.as_view
+    (success_url=reverse_lazy('account:password_change_done')), name='password_change'),
+
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
     path('', views.dashboard, name="dashboard")
     # path('login/', views.auth_login, name='login'),
 ]
