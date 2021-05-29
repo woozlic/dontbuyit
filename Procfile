c179f86d00306066faaeb6bbf238f54ecddac3b6
woozlic <sampaccs@mail.ru>
2021-05-29 16:31:28 +0500

    debug

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: daphne dontbuyit.asgi:application --port $PORT --bind 0.0.0.0 -v2
-worker: python manage.py runworker channel_layer -v2
+worker: python manage.py runworker --settings=dontbuyit.settings -v2
--- dontbuyit/asgi.py
+++ dontbuyit/asgi.py
@@ -1,22 +1,23 @@
 import django
-django.setup()
-
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-from channels.layers import get_channel_layer
-import chat.routing
+from channels.routing import get_default_application
 
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from django.core.asgi import get_asgi_application
+# from channels.layers import get_channel_layer
+# import chat.routing
 
-channel_layer = get_channel_layer()
+# channel_layer = get_channel_layer()
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dontbuyit.settings")
-
-application = ProtocolTypeRouter({
-  "http": get_asgi_application(),
-  "websocket": AuthMiddlewareStack(
-        URLRouter(
-            chat.routing.websocket_urlpatterns
-        )
-    ),
-})
+django.setup()
+application = get_default_application()
+#
+# application = ProtocolTypeRouter({
+#   "http": get_asgi_application(),
+#   "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             chat.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
