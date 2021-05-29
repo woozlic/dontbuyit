f20560f19b3bb9fed00fec34e117b9139c5f4dfc
woozlic <sampaccs@mail.ru>
2021-05-29 12:34:55 +0500

    updated runscript

--- scripts/save_fixtures.py
+++ scripts/save_fixtures.py
@@ -1,11 +1,29 @@
-from items.models import Items
+from items.models import Items, Categories, SubCategories
 
 
 def run():
+    """Save all objects to update fixtures"""
+
     all_objects = Items.objects.all()
     if all_objects:
         for obj in all_objects:
             obj.save()
-        print('All objects saved')
+        print('All items saved')
     else:
-        print('There is no objects to save')
+        print('There is no items to save')
+
+    all_categories = Categories.objects.all()
+    if all_categories:
+        for category in all_categories:
+            category.save()
+        print('All categories saved')
+    else:
+        print('There is no categories to save')
+
+    all_subcategories = SubCategories.objects.all()
+    if all_subcategories:
+        for subcategory in all_subcategories:
+            subcategory.save()
+        print('All subcategories saved')
+    else:
+        print('There is no subcategories to save')
