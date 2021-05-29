4fa81ec6b0a483196de3acf77dd75db03c357e2b
woozlic <sampaccs@mail.ru>
2021-05-29 14:52:54 +0500

    debugging

--- chat/routing.py
+++ chat/routing.py
@@ -5,6 +5,4 @@ from . import consumers
 websocket_urlpatterns = [
     re_path(r'ws/chat/$', consumers.ChatConsumer.as_asgi()),
     re_path(r'ws/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'wss/chat/$', consumers.ChatConsumer.as_asgi()),
-    re_path(r'wss/chat/(?P<room_name>\w+)/$', consumers.ChatConsumer.as_asgi()),
 ]
