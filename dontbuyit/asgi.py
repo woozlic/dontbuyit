dc8956b486cad68340d08f9b48e8309c37d5e918
woozlic <sampaccs@mail.ru>
2021-06-19 19:48:33 +0500

    debug

--- dontbuyit/asgi.py
+++ dontbuyit/asgi.py
@@ -1,8 +1,20 @@
 import os
-# import django
-
-from channels.routing import get_default_application
+import channels
+import django
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dontbuyit.settings")
-# django.setup()
-application = get_default_application()
+django.setup()
+
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
+
+application = ProtocolTypeRouter({
+  "http": get_asgi_application(),
+  "websocket": AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+        )
+    ),
+})
