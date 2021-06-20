0dcbc01463d01e60ce1f0a84340995e9931ca82e
woozlic <sampaccs@mail.ru>
2021-06-20 16:52:02 +0500

    cloudinary

--- dontbuyit/settings.py
+++ dontbuyit/settings.py
@@ -61,10 +61,12 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'cloudinary_storage',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
     'django_extensions',
     'mathfilters',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -163,3 +165,9 @@ CHANNEL_LAYERS = {
 }
 
 django_heroku.settings(locals())
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'YOUR_CLOUD_NAME',
+    'API_KEY': 'YOUR_API_KEY',
+    'API_SECRET': 'YOUR_API_SECRET',
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
